8bd16dcc1eb688be0c34672184da0d02133b4bb5
Wendell47 <ps4.wendell@gmail.com>
2023-12-10 17:34:43 -0400

    [#8]try to fix loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 
+import { useEffect, useState } from 'react'
 import Footer from './components/footer'
 import Header from './components/header'
 import { ContextProvider } from './context/Context'
 import Routes from './routes'
-
+import { Loading } from './pages/songDetailSection/style'
+import Logo from './assets/logo-v3.svg'
 
 function App() {
   
+  const [isLoaded,setIsLoaded] = useState(false)
+
+  window.addEventListener('load',() =>{
+
+      setIsLoaded(true)
+      
+  })
+
+  useEffect(() =>{
+  
+  },[isLoaded])
 
   return (
     <ContextProvider>
+      {!isLoaded ? <Loading id='LoadingScreen'>
+         <img src={Logo} alt="" className='animation_Loading' 
+        />
+
+    </Loading>
+    :
+    <></>
+    }
     <Header/>
     <Routes/>
     <Footer/>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,29 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./App";
-import { useState } from "react";
-import { Loading } from "../pages/songDetailSection/style";
-import Logo from '../assets/logo-v3.svg'
+
+
 
 export default function Routes(){
 
-    const [isLoaded,setIsLoaded] = useState()
-
-    window.addEventListener('load',() =>{
-
-        setIsLoaded(true)
-        
-    })
 
     return(
         <BrowserRouter>
-        {!isLoaded ? <Loading id='LoadingScreen'>
-         <img src={Logo} alt="" className='animation_Loading' 
-        />
-
-    </Loading>
-    :
-    <></>
-    }
+        
             <AppRoutes/>
         </BrowserRouter>
     )
